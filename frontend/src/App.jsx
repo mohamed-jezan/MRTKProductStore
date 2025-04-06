0fb3c074ef445bc9305d671b836c13d71e0ece8b
Jezan-Mohamed <jezanarm@gmail.com>
2025-04-06 14:40:02 +0530

    second commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/create" element={<CreatePage />} />
+        <Route path="/" element={<CreatePage />} />
       </Routes>
     </Box>
   );
